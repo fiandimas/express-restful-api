81d950483dbe3fdad954e0c53df730d300c81310
Alfian Dimas <kidaliez@gmail.com>
2018-09-19 11:32:22 +0700

    add feature to show data

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectID;
+var url = 'mongodb://localhost:27017/';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	MongoClient.connect(url,function(err,db){
+		if(err) throw err;
+		var dbo = db.db('restful-api');
+		dbo.collection('data').find({}).toArray(function(err,result){
+			if(err) throw err;
+			res.send(result);
+			db.close();
+		});
+	});
 });
 
 module.exports = router;
